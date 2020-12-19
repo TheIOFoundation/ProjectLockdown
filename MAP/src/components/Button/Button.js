54e7b037676fe95be8b0fe77fa93c9bf0bd058e3
Dan Watson <daniel.watson@paylinksolutions.co.uk>
2020-12-19 20:57:40 +0000

    Corrected name

--- MAP/src/components/Button/Button.js
+++ MAP/src/components/Button/Button.js
@@ -33,7 +33,7 @@ Button.propTypes = {
    */
   size: PropTypes.oneOf(['small', 'medium', 'large']),
   /**
-   * Map contents
+   * Button contents
    */
   label: PropTypes.string.isRequired,
   /**
