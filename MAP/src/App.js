bb4b5c84eaeaae8ffeef918d9149e8957760ff4e
Dhaiwat10 <dhaiwatpandya@gmail.com>
2021-01-23 21:47:31 +0530

    Add legend display, and automatic dark/light mode detection

--- MAP/src/App.js
+++ MAP/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
-import {useState} from 'react';
-import {Map} from "./components/Map/Map";
-import {LoadingAnimation} from './components/LoadingAnimation/LoadingAnimation';
+import { useEffect, useState } from 'react';
+import { Map } from './components/Map/Map';
+import { LoadingAnimation } from './components/LoadingAnimation/LoadingAnimation';
+import { Legend } from './components/Legend/Legend';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    setIsDark(window.matchMedia('(prefers-color-scheme: dark)').matches);
+  }, []);
 
   return (
     <>
-      <LoadingAnimation isLoading={isLoading}/>
+      <LoadingAnimation isLoading={isLoading} />
       <Map setIsLoading={setIsLoading}></Map>
+      <Legend dark={isDark} />
     </>
   );
 }
