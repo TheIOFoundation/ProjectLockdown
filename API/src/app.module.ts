7bc1bbf01183a1c0f08be91dfad495b1deded5c1
Kevin <maingockien01@gmail.com>
2021-05-19 11:42:20 -0500

    Upadte code style

--- API/src/app.module.ts
+++ API/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DataSetEntriesModule } from './modules';
+import { DataSetEntriesModule } from './endpoints';
 @Module({
-  imports: [DataSetEntriesModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [DataSetEntriesModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
