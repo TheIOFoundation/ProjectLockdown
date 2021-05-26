3704a45bfd46e54f5456687c212a047368191c46
Kevin <maingockien01@gmail.com>
2021-05-26 14:41:21 -0500

    Update testing file structure"

--- API/src/endpoints/datasetentries/datasetentries.models.ts
+++ API/src/endpoints/datasetentries/datasetentries.models.ts
@@ -1,8 +1,8 @@
 export class DataSetEntriesAPI {
-  constructor(
-    public dslUid: string,
-    public startDate: string,
-    public endDate: string,
-    public pldCode: string,
-  ) {}
+    constructor(
+        public dslUid: string,
+        public startDate: string,
+        public endDate: string,
+        public pldCode: string,
+    ) {}
 }
--- API/test/UnitTests/simpletest.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('This is simple test for testing framework', () => {
-  it('This is a simple expect true', () => {
-    expect(true);
-  });
-});
