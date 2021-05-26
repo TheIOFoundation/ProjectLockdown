3f1f27953089bbb191837094f4b25629ab43a547
yehualashet <yehualashetab@gmail.com>
2021-05-26 09:01:05 +0300

    remove mapstateConstant variale

--- MAP/src/utils/constant.js
+++ MAP/src/utils/constant.js
@@ -1,12 +1,3 @@
-
-export const mapStyleConstant = {
-    PARTIAL_LOCK_DOWN: "PARTIAL LOCKDOWN",
-    NO_DATA: "NO DATA",
-    LOCK_DOWN: "LOCKDOWN",
-    DEFAULT: "DEFAULT"
-  }
-
-
 export const UIComponent = {
     TimeSlider: "TimeSlider",
     Legend: "Legend",
