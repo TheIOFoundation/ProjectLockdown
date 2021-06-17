ccf3e300630e87b58876460eb881c79cff70b647
Kevin Mai <40449746+maingockien01@users.noreply.github.com>
2021-06-17 10:14:15 -0500

    Resolve conflict - Edit app.module.ts

--- API/src/app.module.ts
+++ API/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
-import { DataSetEntriesModule } from './modules/';
-=======
 import { DataSetEntriesModule } from './endpoints';
->>>>>>> 646b423ff6135c71716eff10e81662191c55b807
 @Module({
     imports: [DataSetEntriesModule],
     controllers: [AppController],
