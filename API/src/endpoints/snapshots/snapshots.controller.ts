bf31ae148e3ba765ecd5824549ed46ea50d642e9
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-30 20:35:34 +0700

    GET request for data points within a time interval

---
+++ API/src/endpoints/snapshots/snapshots.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { SnapshotsService } from './snapshots.service';
+
+@Controller('snapshots')
+export class SnapshotsController {
+    @Get(':DSLid/:TimestampStart/:TimestampEnd')
+    getSnapshotData(
+        @Param('DSLid') dslId: string,
+        @Param('TimestampStart') timestampStart: string,
+        @Param('TimestampEnd') timestampEnd: string,
+        @Res() res: Response,
+    ) {
+        if (dslId == '2') {
+            return res.status(502).end();
+        } else if (dslId == '1') {
+            const snapshotsService = new SnapshotsService();
+            const selectedSnapshots = snapshotsService.selectSnapshotsByDate(
+                timestampStart,
+                timestampEnd,
+            );
+            return res.status(200).json(selectedSnapshots);
+        }
+    }
+}
