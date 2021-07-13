75598b0c3fa6971e47364d9a319ab5141e660c67
stanley-miao <smiao787@gmail.com>
2021-07-13 14:38:24 -0400

    Add dependencies and functions from StatsBox

--- MAP/src/components/StatsBar/StatsBar.js
+++ MAP/src/components/StatsBar/StatsBar.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./StatsBar.module.scss";
 import { logoSimple, triangleArrow } from "../../assets/icons/icons.js";
+import { toBool } from "../../utils/utils";
+import { useTranslation } from "react-i18next/src";
+import { fetchTotals } from "../../services";
 
-const StatsBar = () => {
+const separateNumber = (number, format) => {
+    const nfObject = new Intl.NumberFormat(format);
+    return nfObject.format(+number);
+};
+
+const StatsBar = ({
+                      dark,
+                      startDate,
+                      endDate,
+                      selectedDate = '2021-01-01',
+                      daysRange = 10
+                  }) => {
+    const [totalsData, setTotalsData] = useState({
+        lockdown: 0,
+        affected: 0,
+    });
+    dark = toBool(dark);
+    const { t } = useTranslation();
+
+    useEffect(() => {
+        fetchTotals(startDate, endDate, selectedDate, daysRange)
+            .then((res) => {
+                setTotalsData(res);
+            })
+            .catch((e) => console.log(e));
+    }, [startDate, endDate, selectedDate, daysRange]);
+
     return (
         <div className={styles.statsBar}>
             <div
@@ -12,16 +41,20 @@ const StatsBar = () => {
             </div>
             <div className={styles.relativeWrapperOne}>
                 <p className={styles.territoriesInLockdown}>
-                    Territories in Lockdown
+                    {t('header.totals.territoriesLockdown')}
                 </p>
-                <p className={styles.num056}>056</p>
+                <p className={styles.num056}>
+                    {totalsData.lockdown}
+                    {/*{separateNumber(totalsData.lockdown, t('languageId'))}*/}
+                </p>
             </div>
             <div className={styles.relativeWrapperTwo}>
                 <p className={styles.num4700000000}>
-                    4,700,000,000
+                    {totalsData.affected}
+                    {/*{separateNumber(totalsData.affected, t('languageId'))}*/}
                 </p>
                 <p className={styles.territoriesInLockdown}>
-                    People Affected
+                    {t('header.totals.peopleAffected')}
                 </p>
             </div>
             <div className={styles.flexWrapperOne}>
