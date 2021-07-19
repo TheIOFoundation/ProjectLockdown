9003f059c860b38baa564f9a9b37fe9e8e5130f7
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-19 19:43:25 +0700

    Put necessary comments

--- API/src/app/Snapshots/snapshots.controller.ts
+++ API/src/app/Snapshots/snapshots.controller.ts
@@ -11,14 +11,20 @@ export class SnapshotsController {
         @Param('TimestampEnd') timestampEnd: string,
         @Res() res: Response,
     ) {
+        // If the DSL id is 2, then it should return nothing other than the appropriate error message
         if (dslId == '2') {
             return res.status(502).send('Invalid DSL id');
-        } else if (dslId == '1') {
+        }
+        // Otherwise, if the DSL id is 1
+        else if (dslId == '1') {
+            // Create an instance of snapshots service
             const snapshotsService = new SnapshotsService();
+            // Select appropriate snapshots data by starting and ending timestamp
             const selectedSnapshots = snapshotsService.selectSnapshotsByDate(
                 timestampStart,
                 timestampEnd,
             );
+            // Return the selected snapshot data
             return res.status(200).json(selectedSnapshots);
         }
     }
