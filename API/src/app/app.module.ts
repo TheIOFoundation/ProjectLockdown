553853baba7bb8a743d7af74a3201af421744f65
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-19 16:01:47 +0700

    Restored it back to the original

--- API/src/app/app.module.ts
+++ API/src/app/app.module.ts
@@ -3,21 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseType, getMetadataArgsStorage } from 'typeorm';
 import { CategoryModule } from './Category';
 import { DataPointModule } from './DataPoint/dataPoint.module';
-import { SnapshotsModule } from './Snapshots/snapshots.module';
-import { SnapshotsController } from './Snapshots/snapshots.controller';
 import { DSLModule } from './DataSetLayer/DSL.module';
 const mongodbConnection: DatabaseType = 'mongodb';
 @Module({
-    controllers: [SnapshotsController],
     imports: [
         DSLModule,
         DataPointModule,
         CategoryModule,
-        SnapshotsModule,
         TypeOrmModule.forRoot({
             type: mongodbConnection as any,
-            url: 'mongodb+srv://Steven:StevenHans@cluster0.s52wk.mongodb.net/ProjectLockdown?retryWrites=true&w=majority',
-            database: 'ProjectLockdown',
+            host: process.env.DB_HOST,
+            database: process.env.DB_DATABASE,
             entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
             synchronize: true,
             useUnifiedTopology: true,
