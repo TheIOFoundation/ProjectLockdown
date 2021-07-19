1b4ec0d5ec8ae798642125be902f383bf0ea8069
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-19 19:14:11 +0700

    Swapped snapshots with controller

--- API/src/app/Snapshots/snapshots.service.ts
+++ API/src/app/Snapshots/snapshots.service.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { snapshot } from '../../../../MAP/public/data/snapshot.json';
 
+// Returned data inside the date has to contain only ISO and Value
 interface IsoValue {
     iso: string;
     value: string;
 }
 
+// Returned snapshot format
 interface Snapshot {
     [date: string]: IsoValue[];
 }
 
 @Injectable()
 export class SnapshotsService {
+    // Convert the date to UNIX timestamp
     convertDateToStamp(date: string): string {
         return new Date(date).getTime().toString();
     }
 
+    // Select the returned snapshot data according to desired date
     selectSnapshotsByDate(
         startTimestamp: string,
         endTimestamp: string,
@@ -23,12 +27,16 @@ export class SnapshotsService {
         let timestamp;
         const result = {};
 
+        // Go through all the dates in snapshot
         for (const date in snapshot) {
+            // Convert the date to its corresponding UNIX timestamp
             timestamp = this.convertDateToStamp(date);
+            // If the timestamp is between the starting and ending timestamp, include it in the result
             if (timestamp >= startTimestamp && timestamp <= endTimestamp) {
                 result[date] = snapshot[date];
             }
         }
+        // Return the resulting data
         return result;
     }
 }
