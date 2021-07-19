18fa5f587f7555205053e89454b32cad85acdf86
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-19 19:08:07 +0700

    Completed necessary comments

--- API/src/app/Snapshots/snapshots.controller.ts
+++ API/src/app/Snapshots/snapshots.controller.ts
@@ -1,25 +1,42 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
-import { Response } from 'express';
-import { SnapshotsService } from './snapshots.service';
+import { Injectable } from '@nestjs/common';
+import { snapshot } from '../../../../MAP/public/data/snapshot.json';
 
-@Controller('snapshots')
-export class SnapshotsController {
-    @Get(':DSLid/:TimestampStart/:TimestampEnd')
-    getSnapshotData(
-        @Param('DSLid') dslId: string,
-        @Param('TimestampStart') timestampStart: string,
-        @Param('TimestampEnd') timestampEnd: string,
-        @Res() res: Response,
-    ) {
-        if (dslId == '2') {
-            return res.status(502).end();
-        } else if (dslId == '1') {
-            const snapshotsService = new SnapshotsService();
-            const selectedSnapshots = snapshotsService.selectSnapshotsByDate(
-                timestampStart,
-                timestampEnd,
-            );
-            return res.status(200).json(selectedSnapshots);
+// Returned data inside the date has to contain only ISO and Value
+interface IsoValue {
+    iso: string;
+    value: string;
+}
+
+// Returned snapshot format
+interface Snapshot {
+    [date: string]: IsoValue[];
+}
+
+@Injectable()
+export class SnapshotsService {
+    // Convert the date to UNIX timestamp
+    convertDateToStamp(date: string): string {
+        return new Date(date).getTime().toString();
+    }
+
+    // Select the returned snapshot data according to desired date
+    selectSnapshotsByDate(
+        startTimestamp: string,
+        endTimestamp: string,
+    ): Snapshot {
+        let timestamp;
+        const result = {};
+
+        // Go through all the dates in snapshot
+        for (const date in snapshot) {
+            // Convert the date to its corresponding UNIX timestamp
+            timestamp = this.convertDateToStamp(date);
+            // If the timestamp is between the starting and ending timestamp, include it in the result
+            if (timestamp >= startTimestamp && timestamp <= endTimestamp) {
+                result[date] = snapshot[date];
+            }
         }
+        // Return the resulting data
+        return result;
     }
 }
