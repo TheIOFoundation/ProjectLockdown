3b5f7b06de22859b3a36883bfd10cf1a5e8be192
stanley-miao <smiao787@gmail.com>
2021-07-27 14:17:53 -0400

    Add new Zoom component

---
+++ MAP/src/components/Zoom/Zoom.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Zoom.module.scss";
+
+const MapControl = () => {
+    return (
+        <div className={styles.mapControl}>
+            <img
+                alt=""
+                className={styles.zoomIn}
+                src="https://static.overlay-tech.com/assets/b9155c78-e032-4122-84b6-97703d62e821.svg"
+            />
+            <div className={styles.zoomDividerLm} />
+            <img
+                alt=""
+                className={styles.zoomOut}
+                src="https://static.overlay-tech.com/assets/d92c7347-d765-4dc2-9128-8297adb7a923.svg"
+            />
+        </div>
+    );
+};
+
+export default MapControl;
