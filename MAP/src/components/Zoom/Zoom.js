e319b0ffc55c0d2532be4d9aa3af9b3b4a371dba
stanley-miao <smiao787@gmail.com>
2021-07-27 15:44:45 -0400

    Use icons in Zoom component

--- MAP/src/components/Zoom/Zoom.js
+++ MAP/src/components/Zoom/Zoom.js
@@ -1,20 +1,21 @@
 import React from "react";
 import styles from "./Zoom.module.scss";
+import { plus, minus } from '../../assets/icons/icons.js';
 
 const MapControl = () => {
     return (
         <div className={styles.zoom}>
-            <img
-                alt=""
-                className={styles.zoomIn}
-                src="https://static.overlay-tech.com/assets/b9155c78-e032-4122-84b6-97703d62e821.svg"
-            />
+            <div className={styles.zoomButton}>
+                <div className={styles.zoomIn}>
+                    {plus}
+                </div>
+            </div>
             <div className={styles.zoomDividerLm} />
-            <img
-                alt=""
-                className={styles.zoomOut}
-                src="https://static.overlay-tech.com/assets/d92c7347-d765-4dc2-9128-8297adb7a923.svg"
-            />
+            <div className={styles.zoomButton}>
+                <div className={styles.zoomOut}>
+                    {minus}
+                </div>
+            </div>
         </div>
     );
 };
