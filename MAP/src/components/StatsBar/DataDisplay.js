78d68c9abd6b9c2160ffa2a0d4a360c2e7640ae7
stanley-miao <smiao787@gmail.com>
2021-07-26 21:44:39 -0400

    Make StatsBar smaller

--- MAP/src/components/StatsBar/DataDisplay.js
+++ MAP/src/components/StatsBar/DataDisplay.js
@@ -15,13 +15,13 @@ const DataDisplay = ({ totalsData }) => {
             <p className={styles.territoriesInLockdown}>
                 {t('header.totals.territoriesLockdown')}
             </p>
-            <p className={styles.numLockdown}>
+            <p className={styles.nums}>
                 {separateNumber(totalsData.lockdown, t('languageId'))}
             </p>
             <p className={styles.peopleAffected}>
                 {t('header.totals.peopleAffected')}
             </p>
-            <p className={styles.numAffected}>
+            <p className={styles.nums}>
                 {separateNumber(totalsData.affected, t('languageId'))}
             </p>
         </div>
