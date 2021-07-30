5aa4712763c27f19ab7cc000584f76f5154fd0f0
yehualashet <yehualashetab@gmail.com>
2021-07-30 15:34:23 +0300

    ✨  add datapoint and category end point in gsheet parser integration

--- DEI/gsheet-parser/src/services/datasetlayers.services.ts
+++ DEI/gsheet-parser/src/services/datasetlayers.services.ts
@@ -1,31 +1,21 @@
 /**
  * Data Model Interfaces
  */
- import DataSetLayer, { IDataSetLayer } from "../models/datasetlayer.model";
- import * as Common from "./common.services";
- import dotenv from 'dotenv';
-import fs from 'fs';
-import axios from "axios";
- 
- import { CreateQuery } from 'mongoose';
- // load .env
-if (fs.existsSync('.env')) {
-  dotenv.config({ path: '.env' });
-}
+ import { IDataSetLayer } from "../models/datasetlayer.model";
 
-const BASEURL = process.env.BASE_URL;
- 
+ import { CreateQuery } from 'mongoose';
+import { insertData } from "./apiservice";
+
  /**
   * Service Methods
   */
   
  export const create = async (newDataSetLayer: CreateQuery<IDataSetLayer>) => {
    
-  return axios.post(`${BASEURL}/DSL`, newDataSetLayer )
-      .then((data) => {
-          return data;
-      }).catch((error: Error) => {
-          throw error;
-      });
+      try {
+        return await insertData('DSL', newDataSetLayer);
+     } catch (error) {
+      throw error;
+     }
  };
  
