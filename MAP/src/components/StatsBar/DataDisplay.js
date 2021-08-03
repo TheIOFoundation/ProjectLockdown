05f6d51ed32823f53fc5d3a3a04c10f4f60d9e80
stanley-miao <smiao787@gmail.com>
2021-08-03 17:21:36 -0400

    Adapt Totals for mobile

--- MAP/src/components/StatsBar/DataDisplay.js
+++ MAP/src/components/StatsBar/DataDisplay.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './DataDisplay.module.scss';
 import { useTranslation } from "react-i18next";
 
@@ -9,6 +9,25 @@ const separateNumber = (number, format) => {
 
 const DataDisplay = ({ totalsData, width }) => {
     const { t } = useTranslation();
+    const [totals, setTotals] = useState('');
+
+    useEffect(() => {
+        let fullTotals = separateNumber(totalsData.affected, t('languageId'));
+        if (width <= 780) {
+            fullTotals = fullTotals.replace(' ','.')
+                .replace(',','.');
+            const cutoffDigit = fullTotals.indexOf('.') + 2;
+            fullTotals = fullTotals.substring(0, cutoffDigit);
+            if (totalsData.affected >= 1000000000) {
+                fullTotals += " B";
+            } else if (totalsData.affected >= 1000000) {
+                fullTotals += " M";
+            } else if (totalsData.affected >= 1000) {
+                fullTotals += " K";
+            }
+        }
+        setTotals(() => fullTotals);
+    }, [t, totalsData.affected, width]);
 
     return (
         <div className={styles.relativeWrapper}>
@@ -24,7 +43,7 @@ const DataDisplay = ({ totalsData, width }) => {
                 {t('header.totals.peopleAffected')}
             </p>
             <p className={styles.nums}>
-                {separateNumber(totalsData.affected, t('languageId'))}
+                {totals}
             </p>
         </div>
     );
