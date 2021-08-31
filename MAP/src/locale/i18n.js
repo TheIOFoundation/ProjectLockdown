da87d8d28fb26dc439b79a9d442286da12de1772
stanley-miao <smiao787@gmail.com>
2021-08-30 20:39:28 -0400

    Use DSL ID to import proper i18n file

--- MAP/src/locale/i18n.js
+++ MAP/src/locale/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import { fetchEnvironments } from "../api";
 
 // import translationEN from './en/index';
 import translationAR from './ar/index';
@@ -13,50 +14,60 @@ import translationZHCN from './zh-CN/index';
 import translationZHHK from './zh-HK/index';
 let translationEN = require('./en/en.json');
 
-translationEN = { ...translationEN, ...require('./en/covid19.json') }
+async function getEnv() {
+    const data = await fetchEnvironments();
+    const dsl_id = data.environment.dsl_id;
+    console.log(dsl_id);
+    switch(dsl_id) {
+        case 1:
+            translationEN = { ...translationEN, ...require('./en/covid19.json') };
+    }
+}
 
-const resources = {
-  en: {
-    translation: translationEN,
-  },
-  es: {
-    translation: translationES,
-  },
-  ar: {
-    translation: translationAR,
-  },
-  it: {
-    translation: translationIT,
-  },
-  pt: {
-    translation: translationPT,
-  },
-  ru: {
-    translation: translationRU,
-  },
-  zhCN: {
-    translation: translationZHCN,
-  },
-  zhHK: {
-    translation: translationZHHK,
-  },
-};
+getEnv().then(() => {
+    const resources = {
+        en: {
+            translation: translationEN,
+        },
+        es: {
+            translation: translationES,
+        },
+        ar: {
+            translation: translationAR,
+        },
+        it: {
+            translation: translationIT,
+        },
+        pt: {
+            translation: translationPT,
+        },
+        ru: {
+            translation: translationRU,
+        },
+        zhCN: {
+            translation: translationZHCN,
+        },
+        zhHK: {
+            translation: translationZHHK,
+        },
+    };
 
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources,
-    fallbackLng: 'en',
-    debug: true,
-    detection: {
-      order: ['queryString', 'cookie'],
-      cache: ['cookie'],
-    },
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+    i18n
+        .use(Backend)
+        .use(LanguageDetector)
+        .use(initReactI18next)
+        .init({
+            resources,
+            fallbackLng: 'en',
+            debug: true,
+            detection: {
+                order: ['queryString', 'cookie'],
+                cache: ['cookie'],
+            },
+            interpolation: {
+                escapeValue: false,
+            },
+        });
+});
 
 export default i18n;
